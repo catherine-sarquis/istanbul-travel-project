09c33496286a7cc38681d9bf1d27f01489e8d7fb
catherine-sarquis <catherinesarquis@gmail.com>
2025-08-02 00:11:36 +0100

    add links

--- scripts.js
+++ scripts.js
@@ -2,12 +2,8 @@ const languageSelect = document.getElementById("language-selection");
 
 languageSelect.onchange = (event) => {
   const selectedOption = event.target.options[event.target.selectedIndex];
-  console.log(selectedOption);
 
   const newUrl = selectedOption.getAttribute("data-url");
-  console.log(newUrl);
 
-  const currentURL = window.location;
-
-  window.location.pathname = "/es/index.html";
+  window.location.href = newUrl;
 };
